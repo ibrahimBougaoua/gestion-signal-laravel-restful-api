fd8027508620134e152dbcf1fb6a53bdc548db2b
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-02 07:28:34 -0700

    Refactoring : user schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
+            $table->string('telephone');
+            $table->string('sexe');
             $table->rememberToken();
             $table->timestamps();
         });
