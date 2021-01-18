41fda392a84a52acecd4fc2c709774d0835cb2d7
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 17:07:31 -0500

    Refactoring : Model Membre

--- app/Membre.php
+++ app/Membre.php
@@ -9,4 +9,33 @@ class Membre extends Model
     protected $fillable = [
         'user_id','equipe_id'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getMembre()
+    {
+        return $this->belongsTo('App\User','id','user_id');
+    }
+
+    public function getTeam()
+    {
+        return $this->belongsTo('App\Equipe','id','equipe_id');
+    }
 }
