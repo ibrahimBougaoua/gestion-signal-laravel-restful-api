167632975fc74a5d4a2b9b72fdbde19a738795cb
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:33:14 -0500

    Refactoring : Model Signalisation

--- app/Signalisation.php
+++ app/Signalisation.php
@@ -9,4 +9,36 @@ class Signalisation extends Model
     protected $fillable = [
         'desc','localisation','lieu','nature','cause','trash','edit'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getInterventions()
+    {
+        return $this->hasMany('App\Intervention','signalisation_id','id');
+    }
+
+    public function getSignales()
+    {
+        return $this->hasMany('App\Signaler','signalisation_id','id');
+    }
+
+
+
 }
