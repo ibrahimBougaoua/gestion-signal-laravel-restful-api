71006e11769e99855f981b0583cb0416d1e1e688
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:43:09 -0500

    Refactoring : Model Informer

--- app/Informer.php
+++ app/Informer.php
@@ -14,4 +14,34 @@ class Informer extends Model
     protected $fillable = [
         'gest_id','chef_id','signalisation_id'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getChefTeam()
+    {
+        return $this->belongsTo('App\User','id','chef_id');
+    }
+
+    public function getManager()
+    {
+        return $this->belongsTo('App\User','id','gest_id');
+    }
+
 }
