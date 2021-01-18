9046bff8793c8a827ced8f88be53e4cb059348a4
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 17:02:57 -0500

    Refactoring : Model Signaler

--- app/Signaler.php
+++ app/Signaler.php
@@ -9,4 +9,33 @@ class Signaler extends Model
     protected $fillable = [
         'user_id','signalisation_id'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getChefTeam()
+    {
+        return $this->belongsTo('App\User','id','user_id');
+    }
+
+    public function getSignalisation()
+    {
+        return $this->belongsTo('App\Signalisation','id','signalisation_id');
+    }
 }
