935c5ab4978cc8295cf82f927e8013198e16b4e3
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:49:00 -0500

    Refactoring : Model Messages

--- app/Messages.php
+++ app/Messages.php
@@ -9,4 +9,33 @@ class Messages extends Model
     protected $fillable = [
         'message','send_user_id','catch_user_id'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getSender()
+    {
+        return $this->belongsTo('App\User','id','send_user_id');
+    }
+
+    public function getCatcher()
+    {
+        return $this->belongsTo('App\User','id','catch_user_id');
+    }
 }
