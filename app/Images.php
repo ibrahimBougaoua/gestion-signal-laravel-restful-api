8387470d5129274e50b18daeb9ef2ac0131cf71d
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:54:49 -0500

    Refactoring : Model Images

--- app/Images.php
+++ app/Images.php
@@ -14,4 +14,33 @@ class Images extends Model
     protected $fillable = [
         'name','size','user_id','signalisation_id'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getAuthor()
+    {
+        return $this->belongsTo('App\User','id','user_id');
+    }
+
+    public function getSignalisation()
+    {
+        return $this->belongsTo('App\Signalisation','id','signalisation_id');
+    }
 }
