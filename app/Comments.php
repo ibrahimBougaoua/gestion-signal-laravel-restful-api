dfbe39cc227300fb86cf12f24d8340bccb766f6b
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:47:00 -0500

    Refactoring : Model Comments

--- app/Comments.php
+++ app/Comments.php
@@ -14,4 +14,39 @@ class Comments extends Model
     protected $fillable = [
         'reply_id','user_id','signalisation_id','name','mail','comment'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getAuthor()
+    {
+        return $this->belongsTo('App\User','id','user_id');
+    }
+
+    public function getSignalisation()
+    {
+        return $this->belongsTo('App\Signalisation','id','signalisation_id');
+    }
+
+    public function getComments()
+    {
+        return $this->hasMany('App\Comments','id','reply_id');
+    }
+
 }
