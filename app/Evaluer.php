821d112f2459148f51c5d59f640ed55dbce68b6e
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:56:17 -0500

    Refactoring : Model Evaluer

--- app/Evaluer.php
+++ app/Evaluer.php
@@ -14,4 +14,33 @@ class Evaluer extends Model
     protected $fillable = [
         'user_id','intervention_id'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getAuthor()
+    {
+        return $this->belongsTo('App\User','id','user_id');
+    }
+
+    public function getIntervention()
+    {
+        return $this->belongsTo('App\Intervention','id','intervention_id');
+    }
 }
