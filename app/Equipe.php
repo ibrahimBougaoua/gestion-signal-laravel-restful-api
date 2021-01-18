c5948c26e2561e57e5a17e762f007a352a6e4fed
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-18 16:40:38 -0500

    Refactoring : Model Equipe

--- app/Equipe.php
+++ app/Equipe.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Equipe extends Model
 {
-    
+
     /**
      * The attributes that are mass assignable.
      *
@@ -15,4 +15,34 @@ class Equipe extends Model
     protected $fillable = [
         'd_f_equipe','mail','telephone','chef_equipe'
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+        'updated_at' => 'datetime:Y-m-d',
+    ];
+
+    public function getMembres()
+    {
+        return $this->hasMany('App\Membre','equipe_id','id');
+    }
+
+    public function getChefTeam()
+    {
+        return $this->belongsTo('App\User','id','chef_equipe');
+    }
+
 }
